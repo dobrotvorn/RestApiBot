9e32670effaefc455b0a2dd4f77c11bc1b37d2ca
Анастасия Петрова <anayupetrova@yandex.ru>
2023-05-30 21:57:30 +0300

    fifth

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 import os
+import requests
 import telebot
 from telebot import types
 
 token = os.getenv("TOKEN")
+weed_token = os.getenv("WEED_TOKEN")
+project = "all"
+api_endpoint = f"https://my-api.plantnet.org/v2/identify/{project}?api-key={weed_token}"
 
 bot = telebot.TeleBot(token)
 
@@ -12,7 +16,7 @@ def start_message(message):
     bot.send_message(message.chat.id, 'Привет')
 
 
-@bot.message_handler(commands=['button'])
+@bot.message_handler(commands=['buttons'])
 def button_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Кнопка Насти")
@@ -20,5 +24,12 @@ def button_message(message):
     markup.add(item1)
     markup.add(item2)
     bot.send_message(message.chat.id, 'Нажмите на понравившуюся кнопку', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def handle_button_nastya(message):
+    if message.text == "Кнопка Насти":
+        bot.send_message(text="Кнопка нажата", chat_id=message.chat.id)
+
 
 bot.polling(none_stop=True)
